a31982b9deda5cf73be17bc0b798e543e8986268
Santiago-Carazo <santiago.carazo@alumnos.upm.es>
2021-05-14 14:56:15 +0200

    Arreglo de menú

--- ARCADE/ARCADE.ino
+++ ARCADE/ARCADE.ino
@@ -13,33 +13,17 @@ int SW;
 
 
 //Definición de las distintas pantallas y sus correspondientes lineas dentro del menu
-LiquidLine linea1(1, 0, "Elegir Jugador");
-//LiquidLine linea2(1, 1, "Controles");
-LiquidLine linea3(1, /*0*/1, "Puntuaciones");
-LiquidScreen pantalla1(linea1, /*linea2,*/ linea3);
 
 LiquidLine linea1_1(1, 0, "Santi");
 LiquidLine linea1_2(1, 1, "Jose");
 LiquidLine linea1_3(1, 0, "Pablo");
-LiquidLine linea1_4(1, 1, "Atras");
-LiquidScreen pantalla2(linea1_1, linea1_2, linea1_3, linea1_4);
-/*
-LiquidLine linea2_1(1, 0, "Atras");
-LiquidScreen pantalla3(linea2_1);
-*/
-LiquidLine linea3_1(1, 0, "Santi");
-LiquidLine linea3_2(1, 1, "Jose");
-LiquidLine linea3_3(1, 0, "Pablo");
-LiquidLine linea3_4(1, 1, "Highest Score");
-LiquidLine linea3_5(1, 0, "Atras");
-LiquidScreen pantalla4(linea3_1, linea3_2, linea3_3, linea3_4/*, linea3_5*/);
+LiquidScreen pantalla1(linea1_1, linea1_2, linea1_3); 
 
-LiquidLine linea4_1(1, 0, "Reanudar");
-LiquidLine linea4_2(1, 1, "Volver a empezar");
-LiquidLine linea4_3(1, 0, "Salir");
-LiquidScreen pantalla5(linea4_1, linea4_2, linea4_3);
+LiquidLine linea2_1(1, 0, "Reanudar");
+LiquidLine linea2_2(1, 1, "Finalizar la partida");
+LiquidScreen pantalla2(linea2_1, linea2_2);
 
-LiquidMenu menu(lcd, pantalla1, pantalla2, /*pantalla3,*/ pantalla4, pantalla5);
+LiquidMenu menu(lcd, pantalla1, pantalla2);
 
 
 void setup() {
@@ -54,68 +38,24 @@ void setup() {
   randomSeed(analogRead(A15));
   
 
-//Asignación de funciones a cada linea del menu
-  linea1.set_focusPosition(Position::LEFT); 
-//  linea2.set_focusPosition(Position::LEFT); 
-  linea3.set_focusPosition(Position::LEFT); 
-   
-  linea1.attach_function(1, fn_jugador); 
-//  linea2.attach_function(1, fn_controles);
-  linea3.attach_function(1, fn_puntuaciones);
-  
-  menu.add_screen(pantalla1);
-  
-
   linea1_1.set_focusPosition(Position::LEFT); 
   linea1_2.set_focusPosition(Position::LEFT); 
   linea1_3.set_focusPosition(Position::LEFT); 
-  linea1_4.set_focusPosition(Position::LEFT); 
-  
+
   linea1_1.attach_function(1, fn_play); 
   linea1_2.attach_function(1, fn_play);
   linea1_3.attach_function(1, fn_play);
-  linea1_4.attach_function(1, fn_atras);
-   
-  menu.add_screen(pantalla2);
   
-/*
-  linea2_1.set_focusPosition(Position::LEFT);
+   menu.add_screen(pantalla1);
   
-  linea2_1.attach_function(1,fn_atras);
-  
-  menu.add_screen(pantalla3);
-*/
 
-  linea3_1.set_focusPosition(Position::LEFT); 
-  linea3_2.set_focusPosition(Position::LEFT); 
-  linea3_3.set_focusPosition(Position::LEFT); 
-  linea3_4.set_focusPosition(Position::LEFT); 
-  linea3_5.set_focusPosition(Position::LEFT); 
-  
-  linea3_1.attach_function(1, fn_santip); 
-  linea3_2.attach_function(1, fn_josep);
-  linea3_3.attach_function(1, fn_pablop);
-  linea3_4.attach_function(1, fn_hs);
-  linea3_5.attach_function(1, fn_atras);
-   
-  menu.add_screen(pantalla4);
-  
-  linea4_1.set_focusPosition(Position::LEFT); 
-  linea4_2.set_focusPosition(Position::LEFT); 
-  linea4_3.set_focusPosition(Position::LEFT); 
-   
-  linea4_1.attach_function(1, fn_play); 
-  linea4_2.attach_function(1, fn_restart);
-  linea4_3.attach_function(1, fn_atras);
-  
-  menu.add_screen(pantalla5);
-
+  linea2_1.set_focusPosition(Position::LEFT); 
+  linea2_2.set_focusPosition(Position::LEFT); 
 
   pantalla1.set_displayLineCount(2);
   pantalla2.set_displayLineCount(2);
-//  pantalla3.set_displayLineCount(2);
-  pantalla4.set_displayLineCount(2);
-  pantalla5.set_displayLineCount(2);
+  
+   menu.add_screen(pantalla2);
   
   menu.set_focusedLine(0);
 
@@ -124,17 +64,17 @@ void setup() {
 
 
 void loop() {
-  Y = analogRead(A1);            // lectura de valor de eje y
+  Y = analogRead(A1);	      		// lectura de valor de eje y
   SW = digitalRead(PULSADOR);   // lectura pulsador
   
   delay(200);
   selectOption();
 
-  if (Y >= 900 && Y <= 1023){     // si el joystick se acciona hacia arriba
+  if (Y >= 900 && Y <= 1023){			// si el joystick se acciona hacia arriba
     menu.switch_focus(false);     // el menu sube
   } 
   
-  if (Y >= 0 && Y <= 100){        // si el joystick se acciona hacia abajo
+  if (Y >= 0 && Y <= 100){			  // si el joystick se acciona hacia abajo
     menu.switch_focus(true);      // el menu baja
   } 
   
@@ -154,46 +94,6 @@ void selectOption(){
     }
   }
 } 
-
-void fn_atras(){
-  menu.change_screen(1);
-  menu.set_focusedLine(0);
-}
-
-void fn_jugador(){
-  menu.change_screen(2);
-  menu.set_focusedLine(0);
-}
-/*
-void fn_controles(){
-  menu.change_screen(3);
-  menu.set_focusedLine(0);
-}
-*/
-void fn_puntuaciones(){
-  menu.change_screen(/*4*/3);
-  menu.set_focusedLine(0);
-}
-
-void fn_santip(){
-  menu.change_screen(1);
-  menu.set_focusedLine(0);
-}
-
-void fn_josep(){
-  menu.change_screen(1);
-  menu.set_focusedLine(0);
-}
-
-void fn_pablop(){
-  menu.change_screen(1);
-  menu.set_focusedLine(0);
-}
-
-void fn_hs(){
-  menu.change_screen(1);
-  menu.set_focusedLine(0);
-}
 
 void fn_restart(){
   fn_play();
@@ -224,18 +124,18 @@ void fn_play(){
     
     
        
-      Y = analogRead(A1);           // lectura de valor de eje y
+      Y = analogRead(A1);	      		// lectura de valor de eje y
   
     
       lc.setLed(0,0,pala-1,false);
       lc.setLed(0,0,pala,false);
       lc.setLed(0,0,pala+1,false);
 
-      if (Y >= 0 && Y < 200 && pala > 1){         // si Y esta en la zona izquierda
+      if (Y >= 0 && Y < 200 && pala > 1){					// si Y esta en la zona izquierda
         pala--;
       } 
   
-      if (Y > 800 && Y <= 1023 && pala < 6){      // si Y esta en la zona derecha
+      if (Y > 800 && Y <= 1023 && pala < 6){			// si Y esta en la zona derecha
         pala++;
       } 
   
